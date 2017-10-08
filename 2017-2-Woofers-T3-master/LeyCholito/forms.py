8b46d42a7d6abd9ac98ef04a9d10f026fb0a0978
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2017-10-07 22:47:06 -0300

    intento de agregar formulario de denuncia

---
+++ 2017-2-Woofers-T3-master/LeyCholito/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class DenunciaForm(forms.Form):
+    opciones = (
+        ("m1", "Abandono en la calle"),
+        ("m2", "Exposición a temperaturas extremas"),
+        ("m3", "Falta de agua"),
+        ("m4", "Falta de comida"),
+        ("m5", "Violencia"),
+        ("m6", "Venta ambulante")
+    )
+
+    maltrato = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=opciones)
--- 2017-2-Woofers-T3-master/LeyCholito/views.py
+++ 2017-2-Woofers-T3-master/LeyCholito/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from .forms import DenunciaForm
 
 # Create your views here.
 
 def index(request):
+
     return render(request, 'index.html')
+
+def formulario(request):
+
+    if request.method == 'POST':
+        form = DenunciaForm(request.POST)
+    return render(request, 'denuncia.html', {'form': form})
